169d238ee78dd1bf50216915d28c18f8259977c9
lonerdoto <kotlirov530@mail.ru>
2022-11-20 16:08:29 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+const title = 'super project';
+const screens = 'Простые, сложные, интерактивные';
+const screenPrice = 10000;
+const rollback = 15;
+const fullPrice = 15000;
+const adaptive = true;
+
+console.log(typeof(title), typeof(fullPrice), typeof(adaptive));
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(screens.toLowerCase().split());
+console.log(`Процент отката посреднику за работу ${fullPrice * rollback / 100} рублей`)
